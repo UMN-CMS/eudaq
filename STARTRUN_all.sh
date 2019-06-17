4bb5c7537d09186dca86652c437eeddca1d36567
Ryan Quinn <quinn.ryanx@gmail.com>
2019-06-17 18:37:23 -0400

    Removed ahcal, dwc, mcp references from STARTRUN.sh

--- STARTRUN_all.sh
+++ STARTRUN_all.sh
@@ -29,38 +29,17 @@ sleep 5
 #################  Data collector #################
 xterm -r -sb -sl 100000 -T "EventnumberSyncData collector" -e 'bin/euCliCollector -n EventnumberSyncDataCollector -t dc1 -r tcp://${HOSTIP}:${RCPORT}; -a tcp://45002; read' &
 xterm -r -sb -sl 100000 -T "EventnumberSyncData collector" -e 'bin/euCliCollector -n EventnumberSyncDataCollector -t dc2 -r tcp://${HOSTIP}:${RCPORT} -a tcp://45001; read' &
-#AHCAL onlinemonitoring 
-xterm -r -sb -sl 100000 -T "directsave data collector" -e 'bin/euCliCollector -n DirectSaveDataCollector -t dc3 -r tcp://${HOSTIP}:${RCPORT}; read' &
-##AHCAL slcio writer
-xterm -r -sb -sl 100000 -T "directsave data collector" -e 'bin/euCliCollector -n DirectSaveDataCollector -t dc4 -r tcp://${HOSTIP}:${RCPORT}; read' &
 
 #################  Producer #################
 #gnome-terminal --geometry=80x600-280-900 -t "CMS HGCal Producer" -e "bash -c \"source ../setup_eudaq_cmshgcal.sh; ./HGCalProducer -r tcp://$HOSTIP:$RCPORT\" " &
 #xterm -r -sb -sl 100000 -geometry 160x30 -T "Ex0-Producer" -e 'bin/euCliProducer -n Ex0Producer -t exo -r tcp://$HOSTIP:$RCPORT |tee -a logs/ex0.log ; read'&
 xterm -r -sb -sl 100000 -geometry 160x30 -T "CMS-HGCAL0" -e 'bin/euCliProducer -n HGCalProducer -t cms-hgcal0 -r tcp://$HOSTIP:$RCPORT '&
-nohup ./bin/euCliProducer -n HGCalProducer -t cms-hgcal1 -r tcp://$HOSTIP:$RCPORT&
-nohup ./bin/euCliProducer -n HGCalProducer -t cms-hgcal2 -r tcp://$HOSTIP:$RCPORT&
-nohup ./bin/euCliProducer -n HGCalProducer -t cms-hgcal3 -r tcp://$HOSTIP:$RCPORT&
-nohup ./bin/euCliProducer -n HGCalProducer -t cms-hgcal4 -r tcp://$HOSTIP:$RCPORT&
-nohup ./bin/euCliProducer -n HGCalProducer -t cms-hgcal5 -r tcp://$HOSTIP:$RCPORT&
-nohup ./bin/euCliProducer -n HGCalProducer -t cms-hgcal6 -r tcp://$HOSTIP:$RCPORT&
-
-################## AHCAL Producer #################
-xterm -r -sb -sl 100000 -geometry 160x30 -T "AHCAL1" -e 'bin/euCliProducer -n AHCALProducer -t AHCAL1 -r tcp://$HOSTIP:$RCPORT | tee -a logs/AHCALProducer_${dt}.log'&
-
-##############  VME Producers on pcminn03 #####################
-printf '\033[22;33m\t killing all EUDAQ  processes on pcminn03 \033[0m \n'
-ssh -Y -T wc "/home/cmsdaq/DAQ/eudaq_v2/KILLRUN.local"  &
-sleep 3
-
-printf '\033[22;33m\t Starting the DWC Producer on pcminn03 \033[0m \n'
-flog="logs/DWC_Producer_$dt.log"
-nohup ssh -Y -T wc "/home/cmsdaq/DAQ/eudaq_v2/bin/euCliProducer -n CMSHGCal_DWC_Producer -t cms-hgcal-dwc -r tcp://${HOSTIP}:${RCPORT};"  > $flog 2>&1 &
-sleep 2
-
-printf '\033[22;33m\t Starting the Digitizer Producer on pcminn03 \033[0m \n'
-flog="logs/Digitizer_Producer_$dt.log"
-nohup ssh -Y -T wc "/home/cmsdaq/DAQ/eudaq_v2/bin/euCliProducer -n CMSHGCal_MCP_Producer -t cms-hgcal-mcp -r tcp://${HOSTIP}:${RCPORT}"  > $flog 2>&1 &
+# nohup ./bin/euCliProducer -n HGCalProducer -t cms-hgcal1 -r tcp://$HOSTIP:$RCPORT&
+# nohup ./bin/euCliProducer -n HGCalProducer -t cms-hgcal2 -r tcp://$HOSTIP:$RCPORT&
+# nohup ./bin/euCliProducer -n HGCalProducer -t cms-hgcal3 -r tcp://$HOSTIP:$RCPORT&
+# nohup ./bin/euCliProducer -n HGCalProducer -t cms-hgcal4 -r tcp://$HOSTIP:$RCPORT&
+# nohup ./bin/euCliProducer -n HGCalProducer -t cms-hgcal5 -r tcp://$HOSTIP:$RCPORT&
+# nohup ./bin/euCliProducer -n HGCalProducer -t cms-hgcal6 -r tcp://$HOSTIP:$RCPORT&
 
 
 ################## OnlineMonitor ##################
@@ -71,10 +50,3 @@ config_file="./user/cmshgcal/conf/onlinemon.conf"
 nohup ./bin/CMSHGCalMonitor  --monitor_name CMSHGCalMonitor --config_file $config_file --reduce 1 --update 1000 --reset -r tcp://$HOSTIP:$RCPORT  --root > $flog 2>&1 &
 
 printf "The logs from the Online Monitor are in $flog file. \n"
-
-
-printf '\033[22;33m\t  Standard (Ahcal) Online Monitor  \033[0m \n'
-flog="./logs/Run${NEWRUNNUM}_StdOnlineMon_$dt.log"
-nohup ./bin/StdEventMonitor -c ahcalOnlineMonitor.conf --monitor_name StdEventMonitor --reset -r tcp://$HOSTIP:$RCPORT > $flog 2>&1 &
-
-printf "The logs from this Monitor are in $flog file. \n"
